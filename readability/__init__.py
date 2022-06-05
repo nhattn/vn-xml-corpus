ffcfb127ac8af348ac12af0ff6ea64610054cd74
Trần Ngọc Nhật <anhchangthode22145@gmail.com>
2022-06-05 22:27:33 +0700

    Định nghĩa hàm lấy dữ liệu từ bên trong chỉ cần gọi

--- readability/__init__.py
+++ readability/__init__.py
@@ -1,3 +1,46 @@
 __version__ = "0.8.2"
 
-from .readability import Document
+import re
+import requests
+import lxml.html
+from .readability import Document as _Document
+
+def get_content(url, agent=None):
+    headers = {"User-Agent": "Mozilla/5.0"}
+    if agent:
+        headers['User-Agent'] = agent
+    response = requests.get(url, headers=headers)
+    doc = _Document(response.text)
+
+    title = doc.title().strip()
+    commonSeparatingCharacters = [' | ', ' _ ', ' - ', '«', '»', '—']
+
+    betterTitle = ''
+    for ch in commonSeparatingCharacters:
+        tmpArray = title.split(ch)
+        if len(tmpArray) > 1:
+            betterTitle = tmpArray[0].strip()
+    if betterTitle and len(betterTitle) > 10:
+        title = betterTitle
+
+    html = re.sub(r'<(a|img|video|source|audio|figure|div|span|strong|em)[^>]*>',' ',doc.summary(True))
+    html = re.sub(r'</(a|img|video|source|audio|figure|div|span|strong|em)[^>]*>',' ',html)
+    html = re.sub(r'<p[^>]*>','<p>',html)
+    html = re.sub(r'<figcaption[^>]*>','<p>',html)
+    html = re.sub(r'</figcaption[^>]*>','</p>',html)
+    html = re.sub(r' {2,}',' ',html)
+    html = html.strip()
+    lines = [ title ]
+    
+    doc = lxml.html.fromstring('<div>%s</div>' % html)
+    for el in doc.xpath('//p'):
+        text = el.text_content().strip()
+        if text:
+            lines.append(text)
+
+    uniqued = []
+    for line in lines:
+        if line not in uniqued:
+            uniqued.append(line)
+
+    return uniqued
